babf1518a73954cf46d902b2d5345c24591c362d
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-02-18 12:25:58 +0200

    task-2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,25 @@
 import { StatisticItem } from 'components';
-import { StatisticsList, StatisticTitle } from './Statistics.styled';
+import { StatisticsList, StatisticTitle, } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+export const Statistics = ({title,stats}) => {
+ const icons =[<FaRegThumbsUp/>,<MdPeople/>,<MdOutlineProductionQuantityLimits/>,<GiTreeDoor/>];
+ 
   return (
     <>
-      <div>StatisticsList</div>
+     {title && <StatisticTitle>{title}</StatisticTitle>} 
+
+<StatisticsList>{stats.map(({id, title,total},index)=>{
+ return <StatisticItem key={id} icon={icons[index]}
+ title={title} total={total}
+  
+/>
+})}
+
+
+</StatisticsList>
     </>
   );
 };
